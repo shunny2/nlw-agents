f6f03590b06ee549fa0afa3e6971428476de7f39
Alexander Davis <alexander.davis.098@gmail.com>
2025-07-09 12:05:44 -0300

    :tada:initial commit

---
+++ backend/src/env.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string().url().startsWith('postgresql://'),
+  FRONTEND_URL: z.string().url().startsWith('http://')
+});
+
+export const env = envSchema.parse(process.env);
