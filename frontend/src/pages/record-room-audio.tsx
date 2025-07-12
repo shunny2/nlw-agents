3ab4a12b8ca1c7bcf8dad269c0ab347cf1dc85f3
Alexander Davis <alexander.davis.098@gmail.com>
2025-07-12 15:17:47 -0300

    :sparkles:feat: added new audio recording room page

---
+++ frontend/src/pages/record-room-audio.tsx
@@ -0,0 +1,118 @@
+import { useRef, useState } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
+
+const isRecordingSupported =
+  !!navigator.mediaDevices &&
+  typeof navigator.mediaDevices.getUserMedia === 'function' &&
+  typeof window.MediaRecorder === 'function';
+
+type AudioParams = {
+  roomId: string;
+};
+
+export function RecordRoomAudio() {
+  const params = useParams<AudioParams>();
+
+  const [isRecording, setIsRecording] = useState(false);
+  const recorder = useRef<MediaRecorder | null>(null);
+  const intervalRef = useRef<NodeJS.Timeout>(null);
+
+  function stopRecording() {
+    setIsRecording(false);
+
+    if (recorder.current && recorder.current.state !== 'inactive') {
+      recorder.current.stop();
+    }
+
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+  }
+
+  async function uploadAudio(audio: Blob) {
+    const formData = new FormData();
+    formData.append('file', audio, 'audio.webm');
+
+    const response = await fetch(
+      `http://localhost:3333/rooms/${params.roomId}/audio`,
+      {
+        method: 'POST',
+        body: formData,
+      }
+    );
+
+    const result = response.json();
+    // biome-ignore lint/suspicious/noConsole: test
+    console.log(result);
+  }
+
+  function createRecorder(audio: MediaStream) {
+    recorder.current = new MediaRecorder(audio, {
+      mimeType: 'audio/webm',
+      audioBitsPerSecond: 64_000,
+    });
+
+    recorder.current.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        uploadAudio(event.data);
+      }
+    };
+
+    recorder.current.onstart = () => {
+      // biome-ignore lint/suspicious/noConsole: teasf
+      console.log('Gravação iniciada!');
+    };
+
+    recorder.current.onstop = () => {
+      // biome-ignore lint/suspicious/noConsole: teasds
+      console.log('Gravação encerrada!');
+    };
+
+    recorder.current.start();
+  }
+
+  async function startRecording() {
+    if (!isRecordingSupported) {
+      alert('O seu navegador não suporta gravação');
+      return;
+    }
+
+    setIsRecording(true);
+
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true,
+        sampleRate: 44_100,
+      },
+    });
+
+    createRecorder(audio);
+
+    intervalRef.current = setInterval(() => {
+      recorder.current?.stop();
+
+      createRecorder(audio);
+    }, 5000);
+  }
+
+  if (!params.roomId) {
+    return <Navigate replace to="/" />;
+  }
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-3">
+      {isRecording ? (
+        <Button className="cursor-pointer" onClick={stopRecording}>
+          Pausar gravação
+        </Button>
+      ) : (
+        <Button className="cursor-pointer" onClick={startRecording}>
+          Iniciar gravação
+        </Button>
+      )}
+      {isRecording ? <p>Gravando...</p> : <p>Pausado</p>}
+    </div>
+  );
+}
